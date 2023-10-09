be60b67d7bccc170648979897193e42bfe06f332
monosniper <vanteateam@gmail.com>
2023-10-09 10:42:41 +0500

    gfdg

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return response()->json(['success' => true]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'errors' => [
+                'email' => 'The provided credentials do not match our records.',
+            ]
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\CoinController;
 use Illuminate\Support\Facades\Route;
 
 Route::domain('api.hogyx.io')->group(function () {
     Route::prefix('v1')->group(function () {
         Route::apiResource('coins', CoinController::class);
+
+        Route::post('login', [AuthController::class, 'login']);
     });
 });
