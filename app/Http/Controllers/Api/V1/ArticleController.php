994747ee03816ba206121380282cffae3593be9f
monosniper <monosniper@gmail.com>
2023-12-08 09:12:55 +0500

    dasd

--- app/Http/Controllers/Api/V1/ArticleController.php
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -33,9 +33,9 @@ class ArticleController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Article $article): ArticleResource
     {
-        //
+        return new ArticleResource($article);
     }
 
     /**
