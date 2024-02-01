8ef011e1f87b3721f9598dc92444d1b74a5c1a54
monosniper <monosniper@gmail.com>
2024-02-01 23:56:47 +0500

    dasd

--- app/Http/Controllers/Api/V1/SessionController.php
+++ app/Http/Controllers/Api/V1/SessionController.php
@@ -39,7 +39,7 @@ class SessionController extends Controller
 
         $user_id = $request->input('user_id');
 
-        Cache::put('sessions.'.$user_id, new SessionResource($session->load('servers.log')));
+        Cache::put('sessions.'.$user_id, new SessionResource($session->load('user_servers.log')));
         Cache::forget('servers.'.$user_id);
         Cache::remember('servers.'.$user_id, 86400, function () use($user_id) {
             return User::find($user_id)->servers;
