bf73900b4674e2fdd637d1d534e127ef52316bf1
monosniper <monosniper@gmail.com>
2024-03-21 23:06:28 +0500

    sada

--- app/Filament/Resources/WalletResource.php
+++ app/Filament/Resources/WalletResource.php
@@ -67,7 +67,7 @@ class WalletResource extends Resource
                     ->money()
                     ->sortable(query: function (Builder $query, string $direction): Builder {
                         return $query
-                            ->orderBy('balance->USDT', $direction);
+                            ->orderBy("JSON_EXTRACT(balance, '$.USDT')", $direction);
                     }),
                 Tables\Columns\TextColumn::make('created_at')
                     ->label('Дата создания')
