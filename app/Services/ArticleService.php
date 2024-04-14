0778971860a828313321e441a0736f6738b87146
monosniper <monosniper@gmail.com>
2024-04-14 21:27:35 +0500

    dasd

--- app/Services/ArticleService.php
+++ app/Services/ArticleService.php
@@ -3,14 +3,10 @@
 namespace App\Services;
 
 use App\Enums\CacheName;
-use App\Enums\CacheType;
 use App\Http\Resources\ArticleResource;
 
 class ArticleService extends CachableService
 {
-
     protected string $resource = ArticleResource::class;
-    protected CacheType $cacheType = CacheType::DEFAULT;
-    protected CacheName $cacheName = CacheName::CONVERTATIONS;
-
+    protected CacheName $cacheName = CacheName::ARTICLES;
 }
