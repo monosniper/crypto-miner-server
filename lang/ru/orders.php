1f25dfdf80e408eb1c821201c236129354a92f6a
monosniper <monosniper@gmail.com>
2024-06-03 02:09:46 +0500

    asdas

--- lang/ru/orders.php
+++ lang/ru/orders.php
@@ -1,23 +1,26 @@
 <?php
 
-use App\Models\Order;
+use App\Enums\OrderMethod;
+use App\Enums\OrderPurchaseType;
+use App\Enums\OrderStatus;
+use App\Enums\OrderType;
 
 return [
     'types' => [
-        Order::DONATE => 'Партнерство',
-        Order::PURCHASE => 'Покупка',
+        OrderType::DONATE->value => 'Партнерство',
+        OrderType::PURCHASE->value => 'Покупка',
     ],
     'methods' => [
-        Order::CRYPTO => 'Криптовалюта',
-        Order::CARD => 'Карта',
+        OrderMethod::CRYPTO->value => 'Криптовалюта',
+        OrderMethod::CARD->value => 'Карта',
     ],
     'purchase_types' => [
-        Order::BALANCE => 'Баланс',
-        Order::SERVER => 'Сервер',
+        OrderPurchaseType::BALANCE->value => 'Баланс',
+        OrderPurchaseType::SERVER->value => 'Сервер',
     ],
     'statuses' => [
-        Order::COMPLETED => 'Оплачено',
-        Order::FAILED => 'Истек срок',
-        Order::PENDING => 'Ожидание оплаты',
+        OrderStatus::COMPLETED->value => 'Оплачено',
+        OrderStatus::FAILED->value => 'Истек срок',
+        OrderStatus::PENDING->value => 'Ожидание оплаты',
     ],
 ];
