9204851c059942af4464b7d7ed2289cf70c35e82
Замалдинов Равиль <vanteateam@gmail.com>
2024-06-18 20:55:05 +0500

    dad

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'v1/users',
+        'v1/quiz',
+        'v1/articles/*',
     ];
 }
