c052ea1785a05a0e7ad532c8c79c5778c403ca76
Radzi7 <vanteateam@gmail.com>
2024-06-21 15:46:54 +0500

    upd

---
+++ app/Filament/Resources/FaqResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FaqResource\Pages;
+use App\Filament\Resources\FaqResource\RelationManagers;
+use App\Models\Faq;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FaqResource extends Resource
+{
+    protected static ?string $model = Faq::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('question')
+                    ->required()
+                    ->label('Вопрос (RU)')
+                    ->maxLength(2000),
+                Forms\Components\RichEditor::make('answer')
+                    ->required()
+                    ->maxLength(65535)
+                    ->label('Ответ (RU)')
+                    ->fileAttachmentsDirectory('faq'),
+                Forms\Components\TextInput::make('question_en')
+                    ->required()
+                    ->label('Вопрос (EN)')
+                    ->maxLength(2000),
+                Forms\Components\RichEditor::make('answer_en')
+                    ->required()
+                    ->maxLength(65535)
+                    ->label('Ответ (EN)')
+                    ->fileAttachmentsDirectory('faq'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('question')
+                    ->label('Вопрос')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFaqs::route('/'),
+            'create' => Pages\CreateFaq::route('/create'),
+            'edit' => Pages\EditFaq::route('/{record}/edit'),
+        ];
+    }
+}
