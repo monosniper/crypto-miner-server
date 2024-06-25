ec0051ec441cd16c77d5a4a58916a1201bc19c19
Замалдинов Равиль <vanteateam@gmail.com>
2024-06-25 19:34:32 +0500

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -20,6 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'http://localhost:5173',
         'http://localhost:3000',
         'https://www.hogyx.io',
         'https://hogyx.io',
