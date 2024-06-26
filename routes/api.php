2cc2a7ee7f8f5523fbafdef6e9eedc1ad501cc01
Radzi7 <vanteateam@gmail.com>
2024-06-26 12:24:51 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -99,75 +99,75 @@ Route::domain('api.hogyx.io')->group(function () {
         });
     });
 
-Route::prefix('v1')
-    ->group(function () {
-        Route::post('users', [AuthController::class, 'register']);
-
-        Route::middleware(AuthenticateOnceWithBasicAuth::class)
-            ->prefix('me')
-            ->group(callback: function () {
-                Route::post('mine', [SessionController::class, 'store']);
-
-                Route::put('/', [UserController::class, 'update']);
-                Route::post('transfer', [TransferController::class, 'store']);
-
-                Route::post('/send-verification-mail', [AuthController::class, 'sendVerificationMail'])
-                    ->middleware('throttle:verification');
-
-                // Static
-                Route::get('/', [UserController::class, 'me']);
-                Route::get('ref', [UserController::class, 'ref']);
-                Route::get('session', UserSessionController::class);
-                Route::get('nfts', UserNftController::class);
-                Route::get('wallet', WalletController::class);
-                Route::get('notifications', NotificationController::class);
-                Route::get('replenishments', ReplenishmentController::class);
-
-                Route::apiResource('servers', ServerController::class)
-                    ->only('index', 'show', 'update');
-
-                Route::apiResource('withdraws', WithdrawController::class)
-                    ->only('index', 'store');
-
-                Route::apiResource('convertations', ConvertationController::class)
-                    ->only('index', 'store');
-
-                Route::apiResource('orders', OrderController::class)
-                    ->except('destroy');
-
-                Route::get('orders/payed/{order}', [OrderController::class, 'payed']);
-            });
-
-        // Other
-        Route::post('quiz', [QuizController::class, 'store'])->middleware('throttle:1,1');
-        Route::post('feedback', [FeedbackController::class, 'store']);
-        Route::post('orders/accept', [OrderController::class, 'markCompleted']);
-        Route::post('orders/reject', [OrderController::class, 'markRejected']);
-
-        // Account
-        Route::middleware('throttle:verification')->group(function () {
-            Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
-            Route::get('verificate/{code}', [AuthController::class, 'verificateMail']);
-        });
-
-        Route::post('check-password-code', [AuthController::class, 'checkPasswordCode']);
-        Route::put('update-password', [AuthController::class, 'updatePassword']);
-        Route::post('check-username', [AuthController::class, 'checkUsername']);
-
-        // Static
-        Route::get('configuration', ConfigurationController::class);
-        Route::get('presets', PresetController::class);
-        Route::get('geo', [AppController::class, 'geo']);
-        Route::get('coins', CoinController::class);
-        Route::get('partners', [AppController::class, 'partners']);
-        Route::get('settings', [AppController::class, 'settings']);
-        Route::get('nfts', NftController::class);
-        Route::apiResource('articles', ArticleController::class)
-            ->only('index', 'show', 'update');
-
-        // WebSockets
-        Route::post('check', [AuthController::class, 'checkToken']);
-        Route::put('user/servers/{server}', [SessionController::class, 'updateServer']);
-        Route::put('user/sessions/{session}/cache', [SessionController::class, 'cacheSession']);
-        Route::apiResource('sessions', SessionController::class);
-    });
+//Route::prefix('v1')
+//    ->group(function () {
+//        Route::post('users', [AuthController::class, 'register']);
+//
+//        Route::middleware(AuthenticateOnceWithBasicAuth::class)
+//            ->prefix('me')
+//            ->group(callback: function () {
+//                Route::post('mine', [SessionController::class, 'store']);
+//
+//                Route::put('/', [UserController::class, 'update']);
+//                Route::post('transfer', [TransferController::class, 'store']);
+//
+//                Route::post('/send-verification-mail', [AuthController::class, 'sendVerificationMail'])
+//                    ->middleware('throttle:verification');
+//
+//                // Static
+//                Route::get('/', [UserController::class, 'me']);
+//                Route::get('ref', [UserController::class, 'ref']);
+//                Route::get('session', UserSessionController::class);
+//                Route::get('nfts', UserNftController::class);
+//                Route::get('wallet', WalletController::class);
+//                Route::get('notifications', NotificationController::class);
+//                Route::get('replenishments', ReplenishmentController::class);
+//
+//                Route::apiResource('servers', ServerController::class)
+//                    ->only('index', 'show', 'update');
+//
+//                Route::apiResource('withdraws', WithdrawController::class)
+//                    ->only('index', 'store');
+//
+//                Route::apiResource('convertations', ConvertationController::class)
+//                    ->only('index', 'store');
+//
+//                Route::apiResource('orders', OrderController::class)
+//                    ->except('destroy');
+//
+//                Route::get('orders/payed/{order}', [OrderController::class, 'payed']);
+//            });
+//
+//        // Other
+//        Route::post('quiz', [QuizController::class, 'store'])->middleware('throttle:1,1');
+//        Route::post('feedback', [FeedbackController::class, 'store']);
+//        Route::post('orders/accept', [OrderController::class, 'markCompleted']);
+//        Route::post('orders/reject', [OrderController::class, 'markRejected']);
+//
+//        // Account
+//        Route::middleware('throttle:verification')->group(function () {
+//            Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
+//            Route::get('verificate/{code}', [AuthController::class, 'verificateMail']);
+//        });
+//
+//        Route::post('check-password-code', [AuthController::class, 'checkPasswordCode']);
+//        Route::put('update-password', [AuthController::class, 'updatePassword']);
+//        Route::post('check-username', [AuthController::class, 'checkUsername']);
+//
+//        // Static
+//        Route::get('configuration', ConfigurationController::class);
+//        Route::get('presets', PresetController::class);
+//        Route::get('geo', [AppController::class, 'geo']);
+//        Route::get('coins', CoinController::class);
+//        Route::get('partners', [AppController::class, 'partners']);
+//        Route::get('settings', [AppController::class, 'settings']);
+//        Route::get('nfts', NftController::class);
+//        Route::apiResource('articles', ArticleController::class)
+//            ->only('index', 'show', 'update');
+//
+//        // WebSockets
+//        Route::post('check', [AuthController::class, 'checkToken']);
+//        Route::put('user/servers/{server}', [SessionController::class, 'updateServer']);
+//        Route::put('user/sessions/{session}/cache', [SessionController::class, 'cacheSession']);
+//        Route::apiResource('sessions', SessionController::class);
+//    });
