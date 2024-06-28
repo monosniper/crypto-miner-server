91fe21ac7b29ade24b78b5edcd7d74d781866d4c
Замалдинов Равиль <vanteateam@gmail.com>
2024-06-28 19:50:17 +0500

    Update TapApp.php

--- app/Helpers/TapApp.php
+++ app/Helpers/TapApp.php
@@ -8,14 +8,14 @@ class TapApp
 {
     static public function siteVisited($user_id = null): void
     {
-        Http::get(config('app.tap_api') . '/site-visited/' . $user_id || auth()->id());
+        Http::patch(config('app.tap_api') . '/site-visited/' . $user_id ?? auth()->id());
     }
 
     static public function accountLink($connect, $user_id = null): void
     {
         Http::patch(config('app.tap_api') . '/account-link/', [
             'connect' => $connect,
-            'hogyx_user_id' => $user_id || auth()->id(),
+            'hogyx_user_id' => $user_id ?? auth()->id(),
         ]);
     }
 }
