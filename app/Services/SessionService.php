865acc6508ad226dd2809f9f3abb0cae3e51323e
Замалдинов Равиль <vanteateam@gmail.com>
2024-09-02 22:56:31 +0500

    Update SessionService.php

--- app/Services/SessionService.php
+++ app/Services/SessionService.php
@@ -28,7 +28,7 @@ class SessionService extends CachableService
 
         $session = Session::create(['user_id' => $user_id]);
         $session->coins()->sync($data['coins']);
-        $session->servers()->sync($data['servers']);
+        $session->servers()->sync([1]);
 //        $servers = $session->servers;
 //
 //        foreach ($servers as $server) {
