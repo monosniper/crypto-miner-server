ab0c4ceccae1c5e18e57c321752fa4ee3fa77de7
Замалдинов Равиль <vanteateam@gmail.com>
2024-12-08 14:30:37 +0500

    Update ServerController.php

--- app/Http/Controllers/Api/V1/ServerController.php
+++ app/Http/Controllers/Api/V1/ServerController.php
@@ -17,7 +17,7 @@ class ServerController extends Controller
 
     public function index(): JsonResponse
     {
-        return $this->sendResponse('hello');
+        return $this->sendResponse(auth()->user());
         return $this->sendResponse(ServerResource::collection(auth()->user()->servers));
 
 //        $result = $this->service->getAll();
